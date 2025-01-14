1964b83ba577aedd9fe7331db18e120340d4860b
Shivam <shivamkonline@gmail.com>
2025-01-14 13:27:01 +0530

    Links changed

--- frontend/src/components/common/ContactComp.jsx
+++ frontend/src/components/common/ContactComp.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import Profile from './Profile'
 import Connect from './Connect'
 import BlogComp from './BlogComp'
+import { Link } from 'react-router-dom';
 
 const ContactComp = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-8 gap-y-8 text-white mt-8">
         <a href='https://bhogul.com/?utm_source=github&utm_medium=social' target='blank' title='Visit Blog' className="group rounded-[2rem] col-span-1 flex items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
-          <p href="#" className='w-full'> <BlogComp /> </p>
+          <p className='w-full'> <BlogComp /> </p>
         </a>
-        <a href='#' target='blank' title='Connect with Me'
+        <Link to='/contact' title='Connect with Me'
           className="group rounded-[2rem] col-span-2 flex items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
           <p href="#" className='w-full'> <Profile /> </p>
-        </a>
-        <a href='#' target='blank' title='Connect with Me'
+        </Link>
+        <Link to='/contact' title='Connect with Me'
           className="group rounded-[2rem] col-span-1 flex items-end p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
           <p href="#" className='w-full'> <Connect /> </p>
-        </a>
+        </Link>
       </div>
 
     )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,10 +17,10 @@ const Home = () => {
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
 
         {/* Info Card */}
-        <a href='#'
-          className="group col-span-1 lg:col-span-2 flex justify-between items-center flex-col rounded-[2rem] row-span-1 lg:row-span-2 lg:h-full p-8 relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
-          <a href="#" className='w-full' title='About Me'> <Intro /> </a>
-        </a>
+        <Link to='/about-me'
+          className="group col-span-1 lg:col-span-2 flex justify-between items-center flex-col rounded-[2rem] row-span-1 lg:row-span-2 lg:h-full p-8 relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer" title='About Me'>
+          <Intro /> 
+        </Link>
 
         {/* Blog Ticker */}
         <div
@@ -29,23 +29,23 @@ const Home = () => {
         </div>
 
         {/* About Me */}
-        <a href='#'
-          className="group rounded-[2rem] col-span-1 flex justify-between items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
-          <a href="#" className='w-full' title='Credentials'> <About /> </a>
-        </a>
+        <Link to='/about-me'
+          className="group rounded-[2rem] col-span-1 flex justify-between items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer" title='Credentials'>
+         <About /> 
+        </Link>
 
         {/* Project */}
-        <a href='#'
-          className="group rounded-[2rem] col-span-1 flex items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
-          <a href="#" className='w-full' title='Projects'> <ProjectComp /> </a>
-        </a>
+        <Link to='/projects'
+          className="group rounded-[2rem] col-span-1 flex items-center p-8 overflow-hidden relative shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer" title='Projects'>
+          <ProjectComp />
+        </Link>
       </div>
 
       {/* Section 2 */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 w-full p-1 mt-8">
-        <a href='#' title='Services' className="group col-span-1 rounded-[2rem] xl:px-12 xl:py-8 p-8 lg:col-span-6 shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
+        <Link to='/services' title='Services' className="group col-span-1 rounded-[2rem] xl:px-12 xl:py-8 p-8 lg:col-span-6 shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735] cursor-pointer">
           <span className='w-full'> <SkillComp /> </span>
-        </a>
+        </Link>
         <div className="col-span-1 rounded-[2rem] xl:px-8 xl:py-8 p-8 lg:col-span-6 shadow-[inset_5px_5px_10px_#0e0e0e,inset_-5px_-5px_10px_#202735]">
           <Counter />
         </div>
